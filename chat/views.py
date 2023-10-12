c6ba5efb0239c534b67348a54d166a5b666d2281
Elvio Severino <elvioseverino@gmail.com>
2023-10-12 09:03:03 -0400

    updated index view and added room view

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request, "chat/chat.html")
+    nick_name = request.COOKIES.get("nick_name")
+
+    print(nick_name)
+    is_nick_name_set = True if nick_name else False
+
+    return render(
+        request, "chat/index.html", {"is_nick_name_set": is_nick_name_set}
+    )
+
+
+def room(request, room_name, nick_name):
+    if request.method == "POST":
+        nick_name = request.POST.get("nick_name")
+        return redirect(f"/chat/{room_name}/{nick_name}")
+
+    else:
+        response = render(
+            request,
+            "chat/room.html",
+            {"room_name": room_name, "nick_name": nick_name},
+        )
+        cookies_nick_name = request.COOKIES.get("nick_name")
+
+        if cookies_nick_name and cookies_nick_name == nick_name:
+            pass
+        else:
+            response.set_cookie("nick_name", nick_name)
+
+        return response
